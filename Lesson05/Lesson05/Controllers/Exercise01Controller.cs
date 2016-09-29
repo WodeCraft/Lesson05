8ca2e13c8f9a7948b54ff4791ac98b34a14ce1a7
WodeCraft <jcr@wodecraft.dk>
2016-09-29 23:19:38 +0200

    Exercise 1

---
+++ Lesson05/Lesson05/Controllers/Exercise01Controller.cs
@@ -0,0 +1,87 @@
+﻿using Lesson05.Infrastructure;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Lesson05.Controllers
+{
+    public class Exercise01Controller : Controller
+    {
+        private List<SelectListItem> countryList = new List<SelectListItem>();
+
+        // GET: Exercise01
+        public ActionResult Index(string countries)
+        {
+            if (Session["countryList"] == null)
+            {
+                countryList.Add(new SelectListItem { Text = "Gambia", Value = "GM" });
+                countryList.Add(new SelectListItem { Text = "Finland", Value = "FI" });
+                countryList.Add(new SelectListItem { Text = "United Kingdom", Value = "UK" });
+                countryList.Add(new SelectListItem { Text = "Denmark", Value = "DK" });
+                countryList.Add(new SelectListItem { Text = "Japan", Value = "JP" });
+                countryList.Add(new SelectListItem { Text = "Jordan", Value = "JO" });
+                countryList.Add(new SelectListItem { Text = "Kenya", Value = "KE" });
+                countryList.Add(new SelectListItem { Text = "France", Value = "FR" });
+                countryList.Add(new SelectListItem { Text = "Czech", Value = "CZ" });
+                countryList.Add(new SelectListItem { Text = "Italy", Value = "IT" });
+                countryList.Add(new SelectListItem { Text = "Norway", Value = "NO" });
+                countryList.Add(new SelectListItem { Text = "Sweden", Value = "SE" });
+                countryList.Add(new SelectListItem { Text = "Estonia", Value = "EE" });
+                Session["countryList"] = countryList;
+            }
+            else
+            {
+                countryList = (List<SelectListItem>)Session["countryList"];
+            }
+
+            ViewBag.Countries = countryList;
+            ViewBag.CountryCode = countries;
+
+            return View();
+        }
+
+        /// <summary>
+        /// What is the difference between calling the method with:
+        /// 
+        /// Index(FormCollection fc)
+        /// 
+        /// And
+        /// 
+        /// Index(string country, string code)
+        /// 
+        /// The result is that the former will reset the TextBoxes, but the latter will leave the values in there. Why is that?
+        /// Does that has something to do with me using the Html.TextBox helper?
+        ///
+        /// </summary>
+        /// <param name="fc"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Index(FormCollection fc)
+        {
+            string country = fc["country"];
+            string code = fc["code"];
+
+            if (Session["countryList"] != null)
+            {
+                countryList = (List<SelectListItem>)Session["countryList"];
+            }
+
+            countryList.Add(new SelectListItem { Text = country, Value = code });
+
+            // Using Linq to sort the list
+            //countryList = countryList.OrderBy(li => li.Text).ToList<SelectListItem>();
+
+            // Using a Lambda expression to sort the list
+            //countryList.Sort((a, b) => a.Text.CompareTo(b.Text));
+
+            // Using the utility class
+            // This method will make it possible to reset the selected item in the list
+            Utilities.SortSelectList(countryList, code);
+
+            ViewBag.Countries = countryList;
+            ViewBag.CountryCode = code;
+
+            return View();
+        }
+
+    }
+}
---
+++ Lesson05/Lesson05/Infrastructure/Utilities.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Lesson05.Infrastructure
+{
+    public class Utilities
+    {
+        /// <summary>
+        /// Why using this when you can do the same by using either Linq or a Lambda expression?
+        /// </summary>
+        /// <param name="selectList"></param>
+        public static void SortSelectList(List<SelectListItem> selectList, string selectedCode)
+        {
+            ArrayList textList = new ArrayList();
+            ArrayList valueList = new ArrayList();
+            foreach (SelectListItem li in selectList)
+            {
+                textList.Add(li.Text);
+            }
+
+            textList.Sort();
+            foreach (object item in textList)
+            {
+                SelectListItem li = selectList.Find(x => x.Text.Contains(item.ToString()));
+                valueList.Add(li.Value);
+            }
+
+            selectList.Clear();
+            for (int i = 0; i < textList.Count; i++)
+            {
+                if (valueList[i].ToString() == selectedCode.ToString())
+                {
+                    selectList.Add(new SelectListItem { Text = textList[i].ToString(), Value = valueList[i].ToString(), Selected = true });
+                }
+                else
+                {
+                    selectList.Add(new SelectListItem { Text = textList[i].ToString(), Value = valueList[i].ToString() });
+                }
+            }
+        }
+    }
+}
